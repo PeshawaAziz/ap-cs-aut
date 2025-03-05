a3c31488534c62d343e2679e1e9f8db6a5f92556
PeshawaAziz <modern3p2017@gmail.com>
2025-03-05 06:24:01 +0330

    feat: implement Person class

--- quera/e4/base/Person.java
+++ quera/e4/base/Person.java
@@ -1,5 +1,26 @@
 package base;
 
-public class Person {
+import java.util.ArrayList;
 
+public class Person {
+    public int id;
+    public String name;
+    public String nationalID;
+
+    public static ArrayList<Person> personList = new ArrayList<>();
+
+    public Person(String name, String nationalID) {
+        this.name = name;
+        this.nationalID = nationalID;
+        id = personList.size();
+        personList.add(this);
+    }
+
+    public static Person findByID(int id) {
+        for (Person person : personList) {
+            if (person.id == id)
+                return person;
+        }
+        return null;
+    }
 }
