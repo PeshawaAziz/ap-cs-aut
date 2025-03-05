63c844445ac6d14efccac526ca1664849d87a54f
PeshawaAziz <modern3p2017@gmail.com>
2025-03-05 08:16:16 +0330

    feat: implement Major class

--- quera/e4/uni/Major.java
+++ quera/e4/uni/Major.java
@@ -1,5 +1,34 @@
 package uni;
 
-public class Major {
+import java.util.ArrayList;
 
+public class Major {
+    public int id;
+    public String name;
+    public final int capacity;
+    public int numberOfStudents = 0;
+
+    public static ArrayList<Major> majorList = new ArrayList<>();
+
+    public Major(String name, int capacity) {
+        this.id = majorList.size();
+        this.name = name;
+        this.capacity = capacity;
+        majorList.add(this);
+    }
+
+    public static Major findByID(int id) {
+        for (Major major : majorList) {
+            if (major.id == id)
+                return major;
+        }
+        return null;
+    }
+
+    public void addStudent() {
+        if (numberOfStudents < capacity) {
+            numberOfStudents++;
+        } else
+            System.out.println("Maximum major capacity reached!");
+    }
 }
