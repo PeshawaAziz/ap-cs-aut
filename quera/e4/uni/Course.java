254f42d843cb3f5e36a79418a8d3fc938d4e6e87
PeshawaAziz <modern3p2017@gmail.com>
2025-03-05 13:51:53 +0330

    feat: implement Course class

--- quera/e4/uni/Course.java
+++ quera/e4/uni/Course.java
@@ -1,5 +1,26 @@
 package uni;
 
-public class Course {
+import java.util.ArrayList;
 
+public class Course {
+    public int id;
+    public int units;
+    public String title;
+
+    public static ArrayList<Course> courseList = new ArrayList<>();
+
+    public Course(String title, int units) {
+        this.id = courseList.size();
+        this.title = title;
+
+        courseList.add(this);
+    }
+
+    public static Course findByID(int id) {
+        for (Course course : courseList) {
+            if (course.id == id)
+                return course;
+        }
+        return null;
+    }
 }
