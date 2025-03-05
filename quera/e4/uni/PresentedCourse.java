9500ee975eaf2fffc563274ef2705d02aadb75b8
PeshawaAziz <modern3p2017@gmail.com>
2025-03-05 14:11:05 +0330

    feat: implement PresentedCourse class

--- quera/e4/uni/PresentedCourse.java
+++ quera/e4/uni/PresentedCourse.java
@@ -1,6 +1,35 @@
 
 package uni;
 
-public class PresentedCourse {
+import java.util.ArrayList;
 
+public class PresentedCourse {
+    public int id;
+    public int courseID;
+    public int professorID;
+    public int capacity;
+
+    public static ArrayList<PresentedCourse> presentedCourseList = new ArrayList<>();
+    public static ArrayList<Integer> studentIDs = new ArrayList<>();
+
+    public PresentedCourse(int courseID, int professorID, int capacity) {
+        this.id = presentedCourseList.size();
+        this.courseID = courseID;
+        this.professorID = professorID;
+        this.capacity = capacity;
+
+        presentedCourseList.add(this);
+    }
+
+    public static PresentedCourse findByID(int id) {
+        for (PresentedCourse presentedCourse : presentedCourseList) {
+            if (presentedCourse.id == id)
+                return presentedCourse;
+        }
+        return null;
+    }
+
+    public void addStudent(int studentID) {
+        studentIDs.add(studentID);
+    }
 }
